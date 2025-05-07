9d0f936fe3468feda6fc44f508f55f9e3f322cfc
Juan Caballero <juandacf@gmail.com>
2025-05-06 19:46:30 -0500

    short experiment

---
+++ domain/factory/IDbFactory.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sgi_app.domain.factory //Este archivo nos permite utilizar las clases 
+{
+    public interface IDbFactory 
+    {
+        
+    }
+}
